370ea4abb27e54217ffbc0c13c7ad619fe558c7b
Assuliya <t.nesterchuk@mail.ru>
2016-08-25 12:41:23 +0300

    return to first template

--- apps/sf_con/apps.py
+++ apps/sf_con/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class UserPokeConfig(AppConfig):
-    name = 'user_poke'
+class SfConConfig(AppConfig):
+    name = 'sf_con'
