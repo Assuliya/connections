c87b04e7110cce1ee14d9cd21c220620c99ee719
Assuliya <t.nesterchuk@mail.ru>
2016-08-25 13:29:12 +0300

    finished the experiment. Returned everything to it's original spot

--- apps/sf_con/urls.py
+++ apps/sf_con/urls.py
@@ -3,6 +3,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^test$', views.test, name='test'),
-    url(r'user/(?P<user_id>\d+)$', views.user_page, name='user_page'),
-
+    
 ]
--- apps/sf_con/views.py
+++ apps/sf_con/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
-from models import User, Poke
+# from models import User, Poke
 import bcrypt
-from collections import Counter
+
 
 def index(request):
     return render(request, 'sf_con/index.html')
 
 def test(request):
     return render(request, 'sf_con/test.html')
-
-def user_page(request, user_id):
-    return render(request, 'sf_con/user.html')
